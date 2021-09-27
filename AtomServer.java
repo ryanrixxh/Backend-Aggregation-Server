a12e5788496ef1419e94c63f3b129b1dfa0d1c10
ryanrixxh <ryanmbr@protonmail.com>
2021-09-27 16:20:03 +0930

    adding extends thread

--- AtomServer.java
+++ AtomServer.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-public class AtomServer {
+public class AtomServer extends Thread {
 
   public static void main(String[] args) {
     ServerSocket server = null;
