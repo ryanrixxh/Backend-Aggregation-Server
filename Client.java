10c64ee1821e47dfb6ad9830307834fd28bace36
ryanrixxh <ryanmbr@protonmail.com>
2021-09-27 16:37:37 +0930

    Adding Client (remains the same as ContentServer)

---
+++ Client.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class Client {
+  public static void main(String[] args) {
+    try (Socket socket = new Socket("localhost", 1234)) {
+
+      //write to AtomServer
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+      //Read from AtomServer
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+      Scanner sc = new Scanner(System.in);
+      String line = null;
+
+      while (!"exit".equalsIgnoreCase(line)) {
+        //Read from user
+        line = sc.nextLine();
+
+        //Send input to AtomServer
+        out.println(line);
+        out.flush();
+
+        //Server reply
+        System.out.println("Server replied: " + in.readLine());
+      }
+
+      sc.close();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
